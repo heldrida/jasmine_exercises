978b360b9328576a6bb5cbc88da2c1f6f87acfd8
heldrida <heldrida@gmail.com>
2014-08-01 15:11:27 +0000

    spie example, using callthrough

---
+++ specs/spies_callthrough.js
@@ -0,0 +1,36 @@
+/*global describe, beforeEach, afterEach, it, expect*/
+
+'use strict';
+
+describe('Track calls and arguments: How to use spies', function () {
+    var foo, bar, fetchedBar;
+
+    beforeEach(function () {
+        foo = {
+	   setBar: function (value) {
+	       bar = value;
+	   },
+	   getBar: function () {
+               return bar;
+           }
+	};
+
+        spyOn(foo, 'getBar').and.callThrough();
+
+        foo.setBar(123);
+        fetchedBar = foo.getBar();
+    });
+
+    it('tracks that the spy was called', function () {
+        expect(foo.getBar).toHaveBeenCalled();
+    });
+
+    it('should not affect other functions', function () {
+        expect(bar).toEqual(123);
+    });
+
+    it('when called returns the requested value', function () {
+        expect(fetchedBar).toEqual(123);
+    });
+
+});
